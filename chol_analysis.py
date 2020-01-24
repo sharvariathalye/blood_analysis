075a880a4d96d92ce1d0f0a4c4771d032fc4477a
sharvariathalye <sharvari.athalye@duke.edu>
2020-01-24 12:39:27 -0500

    Implemented HDL analysis check and output

--- chol_analysis.py
+++ chol_analysis.py
@@ -1,10 +1,19 @@
+def HDL_analysis(HDL_level):
+    if HDL_level >= 60:
+        return "Normal"
+    elif 40 <= HDL_level < 60:
+        return "Borderline low"
+    else: 
+        return "Low" 
+
 def cholesterol_analysis():
     print("Cholesterol Analysis") 
     HDLinput = input("Enter test_result: ") 
     test_info = HDLinput.split("=") 
     if test_info[0] == "HDL":
-        HDL_analysis()
-        
+        answer = HDL_analysis(int(test_info[1]))
+        print("The level is {}".format(answer))
+              
 def interface():
     while True: 
         print("Cholesterol Calculator")
