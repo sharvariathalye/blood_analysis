e16bd7f018644f59b3afc18adf478da01dbc49f7
sharvariathalye <sharvari.athalye@duke.edu>
2020-01-24 12:11:39 -0500

    Built basic outline for interface

---
+++ chol_analysis.py
@@ -0,0 +1,10 @@
+def interface():
+    print("Cholesterol Calculator")
+    print("Options: ")
+    print( "9 -- Quit") 
+    choice = input("enter your option")
+    if choice == '9': 
+    return 
+
+if __name__ == "__main__":
+    interface() 
